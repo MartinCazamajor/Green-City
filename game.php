7475ac20c60d0b94200977c47d7a7a758b341878
Martin Cazamajor <martin.cazamajor@gmail.com>
2019-09-25 17:52:48 +0200

    ajout des images pour la question, non mises en forme

--- game.php
+++ game.php
@@ -1,6 +1,9 @@
 <?php
-$dechets = ["plastique","verre","organique"];
-
+$dechets = [
+        "plastique"=>["img/plastique1.jpg"],
+        "verre"=>["img/glass1.jpg"],
+        "organique"=>["img/organique1.jpg"]
+        ];
 $playerBin = $_GET["bin"];
 $dechet = $_GET["dechet"];
 
@@ -21,17 +24,17 @@ if ($playerBin == ""){
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Title</title>
+    <title>Jeu du tri</title>
     <link rel="stylesheet" type="text/css" href="game.css">
 </head>
 <body>
 <div class="container" id="container">
-    <div class="waste" id="waste"></div>
+    <div class="waste" id="waste"> <img src="<?php echo $dechets[$dechet][0]; ?>" alt="il devrait y avoir un truc ici"> </div>
     <div class="score" id="score"></div>
     <a class="greenBin" href="?bin=green&amp;dechet=<?php echo $_GET["dechet"]; ?>"></a>
     <a class="blueBin" href="?bin=blue&amp;dechet=<?php echo $_GET["dechet"]; ?>"></a>
     <a class="yellowBin" href="?bin=yellow&amp;dechet=<?php echo $_GET["dechet"]; ?>"></a>
-    <a class="reset" href="?bin=&amp;dechet=<?php echo $dechets[rand(0,2)]; ?>">Reset</a>
+    <a class="reset" href="?bin=&amp;dechet=<?php echo array_rand($dechets,1); ?>">Reset</a>
 </div>
 </body>
 </html>
