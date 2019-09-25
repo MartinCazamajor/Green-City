cf34bd4cb35c726f3b3f5f26bbce43c2d5b5907c
Martin Cazamajor <martin.cazamajor@gmail.com>
2019-09-25 17:13:14 +0200

    back-end du jeu fonctionnel

--- game.php
+++ game.php
@@ -1,23 +1,37 @@
 <?php
 $dechets = ["plastique","verre","organique"];
 
-$opponentChoice = "Où faut-il mettre un déchet de type : " . $dechets[rand(0, 2)] . " ?" ."<br>";
+$playerBin = $_GET["bin"];
+$dechet = $_GET["dechet"];
 
-echo "Dans une poubelle verte, bleu ou jaune ?"."<b>";
-
-$playerChoice = readline ("");
-
-if (in_array($playerChoice, $dechets)){
-
-    if ($playerChoice == "verte" && $opponentChoice == "verre") {
-        echo "Bravo !";
-    } elseif ($playerChoice == "jaune" && $opponentChoice == "plastique") {
-        echo "Bravo !";
-    } elseif ($playerChoice == "bleu" && $opponentChoice == "organique") {
-        echo "Bravo !";
-    } else {
-        echo "vous avez perdu";
-    }
+if ($playerBin == ""){
+    echo "Dans quelle poubelle dois-je mettre un dechet de type $dechet ?";
+} elseif ($playerBin == "green" && $dechet == "verre") {
+    echo "Bravo !";
+} elseif ($playerBin == "yellow" && $dechet == "plastique") {
+    echo "Bravo !";
+} elseif ($playerBin == "blue" && $dechet == "organique") {
+    echo "Bravo !";
+} else {
+    echo "Vous avez perdu !";
 }
 
+?>
 
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" type="text/css" href="game.css">
+</head>
+<body>
+<div class="container" id="container">
+    <div class="waste" id="waste"></div>
+    <div class="score" id="score"></div>
+    <a class="greenBin" href="?bin=green&amp;dechet=<?php echo $_GET["dechet"]; ?>"></a>
+    <a class="blueBin" href="?bin=blue&amp;dechet=<?php echo $_GET["dechet"]; ?>"></a>
+    <a class="yellowBin" href="?bin=yellow&amp;dechet=<?php echo $_GET["dechet"]; ?>"></a>
+    <a class="reset" href="?bin=&amp;dechet=<?php echo $dechets[rand(0,2)]; ?>">Reset</a>
+</div>
+</body>
+</html>
